e107b15f063289af73454d4d5c7755bb0d0b0c73
Guilherme Barreto <guilhermeofficial.35@gmail.com>
2022-10-04 18:58:21 -0300

    calculator ready

--- calc.py
+++ calc.py
@@ -4,9 +4,8 @@ import sys
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    return eval(sys.argv[1] + sys.argv[2] + sys.argv[3])
 
 
 if __name__ == "__main__":
     print(main())
-    
